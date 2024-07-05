4dc12f8974a175ebaac5c38db94e4b29ae532542
4yush-Shinde <shindeayush1504@gmail.com>
2024-07-05 10:51:38 +0530

    Upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const projectLinks = document.querySelectorAll('.project-link');
+    
+    projectLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            alert('More information about this project will be available soon.');
+        });
+    });
+});
