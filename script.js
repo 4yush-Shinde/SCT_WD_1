695aa3568c7374e2973c3067f235374fe1f4f1aa
4yush-Shinde <shindeayush1504@gmail.com>
2024-07-20 09:30:17 +0530

    Big Changes

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const projectLinks = document.querySelectorAll('.project-link');
-    
-    projectLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            alert('More information about this project will be available soon.');
-        });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggleButton");
+    const videoBackground = document.getElementById("video-background");
+    const backgroundVideo = document.getElementById("background-video");
+    const body = document.body;
+
+    toggleButton.addEventListener("click", function() {
+        if (videoBackground.style.display === "none") {
+            videoBackground.style.display = "block";
+            backgroundVideo.play();
+            body.classList.remove("show-image");
+        } else {
+            videoBackground.style.display = "none";
+            backgroundVideo.pause();
+            body.classList.add("show-image");
+        }
     });
 });
+
